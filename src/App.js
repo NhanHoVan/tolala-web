7162f1325d919ff815d9b3e925d96209fbd8a744
Van Nhan <nhanhv.qt@gmail.com>
2022-07-24 19:23:32 +0700

    NhanHV - Commit update for layout web

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './Home';
+import Blogs from './Blogs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+      <div className="bgr_app">
+        <div className="bgr_navbar">
+          <Navbar />
+        </div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
